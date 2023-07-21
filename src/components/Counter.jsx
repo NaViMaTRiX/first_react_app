ce50741fff7ce2a70dedb061416ad832dee46fdb
Nikita <81682288+NaViMaTRiX@users.noreply.github.com>
2023-07-21 23:29:25 +0300

    просто

---
+++ src/components/Counter.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    function increment() {
+        setCount(count + 1);
+    }
+
+    function decrement() {
+        setCount(count - 1);
+    }
+
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </>
+    );
+}
+
+export default Counter;
